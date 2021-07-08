26a0974fa1616a7d71b9124fcd72ec8154a613a7
RioRimba <monhero78@gmail.com>
2021-07-08 10:12:47 +0700

    membuat navbar

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts/main')
+
+@section('container')
+    <h1>halaman utama</h1>
+@endsection
---
+++ resources/views/part.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts/main')
+
+@section('container')
+    <h1>halaman part</h1>
+@endsection
